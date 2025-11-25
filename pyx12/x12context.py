536166d9087f7cf6f73b5306a21ca1199a7e8068
ayee <smyee@yahoo.com>
2025-11-25 14:20:48 -0800

    Added fall back for X222 instead of X222A1

--- pyx12/x12context.py
+++ pyx12/x12context.py
@@ -819,8 +819,13 @@ class X12ContextReader(object):
                     if self.map_file != map_file_new:
                         self.map_file = map_file_new
                         if self.map_file is None:
-                            raise pyx12.errors.EngineError("Map not found.  icvn=%s, fic=%s, vriic=%s" %
-                                                           (icvn, fic, vriic))
+                            # Fallback to X222.A1 when specific map not found
+                            fallback_vriic = vriic[:-2] + 'A1' if vriic.endswith('X222') else vriic
+                            map_file_new = self.map_index_if.get_filename(icvn, fallback_vriic, fic)
+                            self.map_file = map_file_new
+                            if self.map_file is None:
+                                raise pyx12.errors.EngineError("Map not found.  icvn=%s, fic=%s, vriic=%s" %
+                                                               (icvn, fic, vriic))
                         cur_map = map_if.load_map_file(self.map_file, self.param, self.map_path)
                         if cur_map.id == '837':
                             self.src.check_837_lx = True
